65390eaf572d6069827a4eddef0324f4a83a00ef
Jarmo Isotalo <jamo@isotalo.fi>
2013-12-01 15:34:58 +0200

    visualizations

--- frontend/app.rb
+++ frontend/app.rb
@@ -1,19 +1,36 @@
 require 'sinatra'
 require 'haml'
 require 'cert_open_data_visualizer'
+require 'json'
+
 class App < Sinatra::Base
   attr_accessor :visualizer, :first_format, :second_format
 
   get '/' do
     @visualizer ||= CertDataVisualizer.new
-    if params[:reload]
-      @visualizer.clean_cache!
-      @visualizer.fetch!
-    end
 
     @first_format ||= @visualizer.first_format
     @second_format ||= @visualizer.second_format
     haml :index
+  end
+
+  get '/charts' do
+    @visualizer ||= CertDataVisualizer.new
+
+    @first_format ||= @visualizer.first_format
+    @second_format ||= @visualizer.second_format
+
+
+    @first_format_json = Hash[@first_format.split("\n").collect { |k| [k.split(" ")[0], k.split(" ")[1] ]}].to_json
+    @second_format_json = Hash[@second_format.split("\n").collect { |k| [k.split(" ")[0], k.split(" ")[1] ]}].to_json
+    haml :charts
+  end
+
+
+  get '/reload' do
+    @visualizer.clean_cache!
+    @visualizer.fetch!
+    redirect to('/')
   end
 
 end
